2304876a0ea6d1dc46da574cc2d0af0f4d6a44b8
Rosa <rdelrioang73@gmail.com>
2024-02-20 00:37:41 +0100

    Handling Events 2

--- src/MouseClicker.jsx
+++ src/MouseClicker.jsx
@@ -1,8 +1,18 @@
 export function MouseClicker(){
+
     function handleClick(event){
         console.log(event.target.name)
     }
+
+    function handleImagenClick(event){
+        console.log("Imagen src: ", event.target.src)
+        {/*Se puede evitar la propagación del evento con: */}
+        event.stopPropagation()
+    }
     return(
-        <button name="uno" onClick={handleClick}>Púlsame</button>
+        <button name="uno" onClick={handleClick}>
+            <img src="imagen.jpg" alt="imagen" onClick={handleImagenClick} />
+            Púlsame :D
+        </button>
     )
 }
