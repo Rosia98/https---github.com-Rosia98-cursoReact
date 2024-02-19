16e2c01edf3486042d5b06ce8cd2cf3cb763c765
Rosa <rdelrioang73@gmail.com>
2024-02-19 19:11:42 +0100

    Condionales 1

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -4,7 +4,18 @@ export function Welcome({name, age}){
     return(
         <div>
             <p>Welcome, <strong>{name}</strong>!</p>
-            <Age age={25}/>
+            {/*Mayor que 18*/}
+            {age > 18 && <Age age={age} />}
+
+            {/* Si age está presente */}
+            {age && <Age age={age} />}
+
+            {/* Si está entre 18 y 65 */}
+            {age > 18 && age < 65 && <Age age={age} />}
+
+            {/* Si age está entre 18 y 65 y el name es "John" */}
+            {age > 18 && age < 65 && name === "John" && <Age age={age} />}
+            
         </div>
         
     )
