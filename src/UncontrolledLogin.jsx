946034c95aa2153513b4f49b8fa451730b06ab40
Rosa <rdelrioang73@gmail.com>
2024-02-20 18:02:32 +0100

    Formularios no controlados 2

--- src/UncontrolledLogin.jsx
+++ src/UncontrolledLogin.jsx
@@ -2,10 +2,21 @@
 export function UncontrolledLogin({onLogin}){
     function handledSubmit(event){
         event.preventDefault();
+        {/*
+        Ventajas:
+            -Su uso fácil para recopilar datos de los formularios
+            -Manejar archivos, se pueden manchar ficheros al servidor juntos con los datos del form
+            -Soporte para diferentes codificaciones de carácteres de forma automática
+        
+        Desventajas:
+            -Se complica para campos no estándar del modelos html
+            -No es tan explícito para rastrear y gestionar los datos del formulario
+            -Es necesario manipular el documento DOM para acceder a los valores*/}
+        const formData = new FormData(event.target)
 
-        const username= event.target.elements.usernameInput.value
-        const password= event.target.elements.passwordInput.value
-        const remember= event.target.elements.rememberInput.checked
+        const username= formData.get('usernameInput')
+        const password= formData.get('passwordInput')
+        const remember= formData.get('rememberInput')=== 'on'
 
         onLogin({username, password, remember})
     }
@@ -18,16 +29,17 @@ export function UncontrolledLogin({onLogin}){
     return(
         <form action="#" onSubmit={handledSubmit}>
             <label htmlFor="usernameInput">Username:</label>
-            <input type="text" id="usernameInput" />
+            <input type="text" id="usernameInput" name="usernameInput"/>
 
             <label htmlFor="passwordInput">Password:</label>
-            <input type="password" id="passwordInput" />
+            <input type="password" id="passwordInput" name="passwordInput" />
 
             <label>
-                <input type="checkbox" id="rememberInput" />
+                <input type="checkbox" id="rememberInput" name="rememberInput"/>
                 Remember me?
             </label>
-            <button type="burron" onClick={handleReset}>Reset</button>
+            <button type="submit">Login</button>
+            <button type="button" onClick={handleReset}>Reset</button>
         </form>
     )
 }
