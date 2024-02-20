b0992de7dc027708d155fee678cded3747a80fa9
Rosa <rdelrioang73@gmail.com>
2024-02-20 23:26:17 +0100

    SWR3

--- src/useGithubUser.jsx
+++ src/useGithubUser.jsx
@@ -12,8 +12,13 @@ export function useGithubUser(username){
   const apiUrl =  username ? `https://api.github.com/users/${username}` : null
   const { data, error } = useSWR(apiUrl, fetcher)
 
+  function refetchUser(){
+    // Manually trigger a re-fetch of the data
+    revalidate();
+  };
   return {  
     user: data,
     isLoading: !error && !data,
-    isError: error }
+    isError: error,
+    refetchUser, }
 }
