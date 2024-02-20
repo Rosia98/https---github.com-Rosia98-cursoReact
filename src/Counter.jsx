80dd96b3ae373e6483fce6e7edc2f00397e64cee
Rosa <rdelrioang73@gmail.com>
2024-02-20 18:35:48 +0100

    Ref 3

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,16 +1,16 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { CounterDisplay } from "./CounterDisplay";
 {/*Se puede usar el useState para  gestionar el valor del componente,
  al llamar al valor setter deberá ser una función porque sabremos que estamos consiguiendo el valor más reciente*/}
 export function Counter({initialValue=0, increment=1, decrement=1, reset=0}){
     const[count, setCount]= useState(initialValue);
+    const directionRef=useRef(null)
 
     useEffect(()=>{
-        
-        return()=>{
-            console.log(`El valor del contador es: ${count}`)
-        }
-    },[count])
+        directionRef.current = count > initialValue ? 'arriba' : 'abajo'
+
+        console.log('Dirección: ', directionRef.current)
+    },[count, initialValue])
 
     function handlerIncrementCounter(){
         setCount((c)=>{
